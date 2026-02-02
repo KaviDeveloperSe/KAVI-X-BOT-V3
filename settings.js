0744198b56320873cdb3e8b48d740de6d2d86cea
KaviDeveloperSe <cyberkavi011@gmail.com>
2026-02-02 10:59:55 +0530

    init

--- settings.js
+++ settings.js
@@ -12,8 +12,8 @@ global.author = process.env.AUTHOR || "Cyber Kavi"
 global.creator = process.env.CREATOR || "94702128378@s.whatsapp.net"
 global.botprefix = process.env.BOTPREFIX || "."
 global.restart = process.env.RESTART || true
-global.websitex = process.env.WEBSITEX || "https://kavi-x-bot-login.up.railway.app/"
-global.wagc = process.env.WAGC || "https://chat.whatsapp.com/GgMPvG6VSeJCOeyJfe6TDS"
+global.websitex = process.env.WEBSITEX || "https://kavi-x-bot-login.up.railway.app"
+global.wagc = process.env.WAGC || "https://moviemixtapesocial.zone.id"
 global.botscript = process.env.BOTSCRIPT || "https://github.com/KaviDeveloperSe/KAVI-X-BOT"
 global.botlogo = process.env.BOTLOGO || "https://raw.githubusercontent.com/KaviDeveloperSe/KAVI-X-BOT-DB/refs/heads/main/kavix03.png"
 
